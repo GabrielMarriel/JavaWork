fbf1ad0eb4e7572c27579f710e7f48b23964e52a
GabrielMarriel <159497987+GabrielMarriel@users.noreply.github.com>
2024-09-18 18:26:48 -0300

    Update TesteDeConta.java
    I added some methods that are missing

--- Gabriel_Documentos/src/TrabalhoJava2/TesteDeConta.java
+++ Gabriel_Documentos/src/TrabalhoJava2/TesteDeConta.java
@@ -3,7 +3,11 @@ package TrabalhoJava2;
 public class TesteDeConta {
     public static void main(String[] args) {
         ContaCorrente conta = new ContaCorrente();
-        conta.Inicializacao(33, 1346, "Joao", "10/02/3007", 354872437);
+        
+        conta.Inicializacao();
+        conta.Sacar();
         conta.verificar_saldo();
+        conta.transferir();
+        conta.cancelarConta();
     }
 }
